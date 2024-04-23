2816033244ff7633e6233e7e7215030dd2922261
Timur <Stafr95@mail.ru>
2024-04-23 18:12:19 +0500

    Create interfaces for Task and List

--- src/store/types.ts
+++ src/store/types.ts
@@ -17,3 +17,19 @@ export const UNSET_TASK_TO_EDIT = 'UNSET_TASK_TO_EDIT';
 
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 
+export interface Task {
+		name: string;
+		id: string;
+		completed: boolean;
+}
+
+export interface List {
+		name: string;
+		id: string;
+		tasks: Task[];
+}
+
+export interface Lists {
+		[id: string]: List
+}
+
