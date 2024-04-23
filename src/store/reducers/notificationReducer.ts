5cea70e854828368b68aeda572f1879e6b6fbf1e
Timur <Stafr95@mail.ru>
2024-04-23 20:08:25 +0500

    Create reducers to combine them into rootReducer

---
+++ src/store/reducers/notificationReducer.ts
@@ -0,0 +1,8 @@
+import { NotificationAction, NotificationState } from "../types";
+
+export default (state, action: NotificationAction): NotificationState => {
+		switch (action.type) {
+				default:
+						return state;
+		}
+}
