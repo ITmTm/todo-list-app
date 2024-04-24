97c222325ad152843ab2144751104d1d1df9a32a
Timur <Stafr95@mail.ru>
2024-04-24 16:19:02 +0500

    Make store available in the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+
+import '../node_modules/bulma/css/bulma.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
 import App from './App';
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+				<App />
+		</Provider>
   </React.StrictMode>
 );
