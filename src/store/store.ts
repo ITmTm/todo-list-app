a74c58c5a573e9dfe05629352bf3806769203c8a
Timur <Stafr95@mail.ru>
2024-04-24 16:56:04 +0500

    Importing dependencies

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,16 @@
-import  { legacy_createStore as createStore, combineReducers } from 'redux';
-import { composeWithDevTools} from '@redux-devtools/extension';
+import { combineReducers, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-import listReducer from "./reducers/listReducer";
-import notificationReducer from "./reducers/notificationReducer";
+import listReducer from './reducers/listReducer';
+import notificationReducer from './reducers/notificationReducer';
+
 
 const rootReducer = combineReducers({
 		list: listReducer,
 		notification: notificationReducer
 });
 
+// @ts-ignore
 const store = createStore(rootReducer, composeWithDevTools());
 
 export type RootState = ReturnType<typeof rootReducer>;
