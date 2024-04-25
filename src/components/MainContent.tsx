6f10905e9634ca6f76705d1269d769c35942e95e
Timur <Stafr95@mail.ru>
2024-04-25 19:22:09 +0500

    Import useSelector and use AddNewTask component

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
+import { useSelector } from "react-redux";
+
 import SelectList from "./SelectList";
+import { RootState } from "../store/store";
+import AddNewTask from "./AddNewTask";
 
 const MainContent: FC = () => {
+		const selectedList = useSelector((state: RootState) => state.list.selectedList);
+
 		return (
 				<div className="column is-9">
 						<div className="box">
 								<SelectList />
+								{
+										selectedList &&
+											<>
+													<AddNewTask list={selectedList} />
+											</>
+								}
 						</div>
 				</div>
 		);
