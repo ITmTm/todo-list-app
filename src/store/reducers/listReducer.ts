b05fed0ba03033377a69649d503854e29026831a
Timur <Stafr95@mail.ru>
2024-04-25 17:08:05 +0500

    Add cases fpr tasks in list reducer

--- src/store/reducers/listReducer.ts
+++ src/store/reducers/listReducer.ts
@@ -1,6 +1,8 @@
 import {
 		ADD_LIST,
+		ADD_TASK,
 		DELETE_LIST,
+		DELETE_TASK,
 		GET_LIST_BY_ID,
 		GET_LISTS,
 		Lists,
@@ -8,7 +10,12 @@ import {
 		ListState,
 		SET_LIST_TO_EDIT,
 		SET_LISTID_TO_DELETE,
-		UPDATE_LIST
+		SET_SELECTED_LIST,
+		SET_TASK_TO_DELETE,
+		SET_TASK_TO_EDIT,
+		UNSET_TASK_TO_DELETE,
+		UNSET_TASK_TO_EDIT,
+		UPDATE_LIST, UPDATE_TASK
 } from "../types";
 
 const initialState: ListState = {
@@ -75,7 +82,7 @@ export default (state = initialState, action: ListsAction): ListState => {
 
 				case DELETE_LIST:
 						const clonedListsFromLS2 = {...listsFromLS};
-						const listId = clonedListsFromLS2[action.payload].id
+						const listId = clonedListsFromLS2[action.payload].id;
 						delete clonedListsFromLS2[action.payload];
 						saveListsToLS(clonedListsFromLS2);
 						return {
@@ -95,6 +102,88 @@ export default (state = initialState, action: ListsAction): ListState => {
 								lists: clonedListsFromLS3,
 								listToEdit: null
 						}
+
+				case SET_SELECTED_LIST:
+						const selectedList = getListsFromLS()[action.payload];
+						return {
+								...state,
+								selectedList: selectedList
+						}
+
+				case ADD_TASK:
+						const clonedListsFromLS4 = {...listsFromLS};
+						clonedListsFromLS4[action.payload.list.id].tasks.push(action.payload.task);
+						saveListsToLS(clonedListsFromLS4);
+						return {
+								...state,
+								lists: clonedListsFromLS4,
+								selectedList: clonedListsFromLS4[action.payload.list.id]
+						}
+
+				case SET_TASK_TO_DELETE:
+						return {
+								...state,
+								taskToDelete: {
+										task: action.payload.task,
+										list: action.payload.list
+								}
+						}
+
+				case UNSET_TASK_TO_DELETE:
+						return {
+								...state,
+								taskToDelete: null
+						}
+
+				case DELETE_TASK:
+						const clonedListsFromLS5 = {...listsFromLS};
+						const clonedTasks = [...clonedListsFromLS5[state.taskToDelete!.list.id].tasks];
+						const task = clonedTasks.find(task => task.id === state.taskToDelete!.task.id);
+						clonedTasks.slice(clonedTasks.indexOf(task!), 1);
+						clonedListsFromLS5[state.taskToDelete!.list.id].tasks = clonedTasks;
+						saveListsToLS(clonedListsFromLS5);
+						return {
+								...state,
+								lists: clonedListsFromLS5,
+								selectedList: clonedListsFromLS5[state.taskToDelete!.list.id],
+								taskToDelete: null
+						}
+
+				case SET_TASK_TO_EDIT:
+						return {
+								...state,
+								taskToEdit: {
+										task: action.payload.task,
+										list: action.payload.list
+								}
+						}
+
+				case UNSET_TASK_TO_EDIT:
+						return {
+								...state,
+								taskToEdit: null
+						}
+
+				case UPDATE_TASK:
+						const clonedListsFromLS6 = {...listsFromLS};
+						const clonedList = {...clonedListsFromLS6[action.payload.list.id]};
+						const clonedTasks2 = [...clonedList.tasks];
+						const task2 = clonedTasks2.find(task => task.id === action.payload.taskId);
+						const clonedTask = {...task2!};
+						clonedTask.name = action.payload.taskName;
+						clonedTask.completed = action.payload.taskState;
+						const updatedTasks = clonedTasks2.map(task => task.id === clonedTask.id ? clonedTask : task);
+						clonedList.tasks = updatedTasks;
+						clonedListsFromLS6[clonedList.id] = clonedList;
+						saveListsToLS(clonedListsFromLS6);
+
+						return {
+								...state,
+								lists: clonedListsFromLS6,
+								selectedList: clonedList,
+								taskToEdit: null
+						}
+
 
 				default:
 						return state;
