a48981ad63f01a5ba13105d20f4755770b51f2f0
Timur <Stafr95@mail.ru>
2024-04-26 16:46:06 +0500

    Creation component Notification

---
+++ src/components/Notification.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { setNotification } from "../store/actions";
+import { RootState } from "../store/store";
+
+interface NotificationProps {
+		msg: string;
+}
+
+let timeout: ReturnType<typeof setTimeout>;
+
+const Notification: FC<NotificationProps> = ({ msg }) => {
+		const dispatch = useDispatch();
+		const type = useSelector((state: RootState) => state.notification.type);
+
+		useEffect(() => {
+				if (msg !== '') {
+						if (timeout) {
+								clearTimeout(timeout);
+						}
+						timeout = setTimeout(() => {
+								// @ts-ignore
+								dispatch(setNotification(''));
+						}, 3000);
+				}
+		}, [dispatch, msg]);
+
+		const closeNotification = () => {
+				// @ts-ignore
+				dispatch(setNotification(''));
+				clearTimeout(timeout);
+		}
+
+		return (
+				<div className={msg ? `${type === 'danger' ? 'notification is-danger' : 'notification is-primary'}` : 'notification is-hidden'}>
+						<button
+								className="delete"
+								onClick={closeNotification}></button>
+						<p>{msg}</p>
+				</div>
+		);
+}
+
+export default Notification;
